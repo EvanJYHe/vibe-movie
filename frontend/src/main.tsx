09adb6836f7d3b32a61756b9f5111309123be6c5
Evan He <evan.he24@gmail.com>
2026-08-02 05:07:28 -0400

    Add web analytics integrations

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 
 const normalizedPath = window.location.pathname.replace(/\/+$/, "") || "/";
@@ -15,6 +16,7 @@ async function renderRoute() {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <Route />
+      <Analytics />
     </StrictMode>
   );
 }
